1e536eaab8fbe40d52bb1d25b72f3947ddd1d3ad
ナオ <jpc.70.p@gmail.com>
2022-04-16 16:17:33 +0900

    登録完了画面

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Http\Request;
+use DB;
 
 class RegisterController extends Controller
 {
@@ -100,6 +101,10 @@ class RegisterController extends Controller
     }
 
     public function added(){
-        return view('auth.added');
+        $newuser = DB::table('users')
+            ->latest()
+            ->first();
+
+        return view('auth.added', ['newuser' => $newuser]);
     }
 }
