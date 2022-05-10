ec7a326bdf8c9cb6cc149f89e353d1f2aad10775
ナオ <jpc.70.p@gmail.com>
2022-05-10 21:56:27 +0900

    共通パーツ

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,30 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\View;
+use Auth;
+use DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct(){
+        $this->middleware(function ($request, $next) {
+
+            $followcount = DB::table('follows')
+              ->where('follower', Auth::id())
+              ->count();
+
+            $followercount = DB::table('follows')
+              ->where('follow', Auth::id())
+              ->count();
+
+            // viewに共通データを渡す
+            View::share('followcount', $followcount);
+            View::share('followercount', $followercount);
+
+            return $next($request);
+        });
+    }
 }
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -22,10 +22,10 @@
 <body>
     <header>
         <div id = "head">
-        <h1><a><img src="images/logo.png"></a></h1>
+        <h1><a><img src="{{ asset('images/main_logo.png') }}"></a></h1>
             <div id="">
                 <div id="">
-                    <p>〇〇さん<img src="images/arrow.png"></p>
+                    <p>{{ Auth::user()->username }}</p>
                 <div>
                 <ul>
                     <li><a href="/top">ホーム</a></li>
@@ -41,15 +41,15 @@
         </div >
         <div id="side-bar">
             <div id="confirm">
-                <p>〇〇さんの</p>
+                <p>{{ Auth::user()->username }}さんの</p>
                 <div>
                 <p>フォロー数</p>
-                <p>〇〇名</p>
+                <p>{{ $followcount }}名</p>
                 </div>
                 <p class="btn"><a href="/followlist">フォローリスト</a></p>
                 <div>
                 <p>フォロワー数</p>
-                <p>〇〇名</p>
+                <p>{{ $followercount }}名</p>
                 </div>
                 <p class="btn"><a href="/followerlist">フォロワーリスト</a></p>
             </div>
